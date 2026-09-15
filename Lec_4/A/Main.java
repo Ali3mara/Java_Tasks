39052bcf617d442a801f653c939de3ce6c8fc78b
Ali Emara <aliemara2006@gmail.com>
2026-09-15 18:38:49 +0300

    Added some file to run the project. Renamed from main.java to Main.java, with capital M.

--- Lec_4/A/Main.java
+++ Lec_4/A/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         nameGen nGen = new nameGen();
-        System.out.println("Emter the name:)");
+        System.out.println("Enter the name: ");
         String n = scan.next();
         nGen.nameGen(n);
     }
